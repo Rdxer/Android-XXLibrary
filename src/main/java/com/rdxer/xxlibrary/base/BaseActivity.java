278fb6e2eeab561b71b73a522d1290de430d061f
LXF <rdxer@foxmail.com>
2016-06-21 17:53:43 +0800

    正向传值 && 逆向传值封装完毕

--- src/main/java/com/rdxer/xxlibrary/base/BaseActivity.java
+++ src/main/java/com/rdxer/xxlibrary/base/BaseActivity.java
@@ -1,10 +1,10 @@
 package com.rdxer.xxlibrary.base;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
-import com.litesuits.orm.LiteOrm;
+import com.rdxer.xxlibrary.utils.Log;
 
 
 /**
@@ -13,18 +13,25 @@ import com.litesuits.orm.LiteOrm;
 
 public abstract class BaseActivity extends Activity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+//        getIntent().getExtras().getString(KEY_Activity_Parames_Cache);
 
-        // add
     }
 
     @Override
     protected void onDestroy() {
         // del
+        Log.e(getClass() + "onDestroy");
+        getApp().removeParamsFromJumpCache(getKEY_Activity_Parames());
         super.onDestroy();
     }
+
+    //********************************************
+    //                常用的方法
+    //********************************************
 
     /**
      * 在子线程运行,
@@ -37,6 +44,100 @@ public abstract class BaseActivity extends Activity {
         thread.start();
     }
 
+    /**
+     * @return 全局单例 application
+     */
+    public BaseApplication getApp() {
+        return BaseApplication.getShared();
+    }
+
+    //********************************************
+    //                跳转的参数处理
+    //********************************************
+    // 随机的一个 key
+    public static final String KEY_Activity_Parames_Cache = "Activity_Parames_Cache_9f080b39-f811-4da1-b7aa-a2fdc66dd5c3";
+
+    /**
+     * 携带参数跳转
+     *
+     * @param intent  意图
+     * @param parames 参数随意类型,
+     */
+    public void startActivity(Intent intent, Object parames) {
+        String key = getApp().putParamsToJumpCache(parames);
+        intent.putExtra(KEY_Activity_Parames_Cache, key);
+        super.startActivity(intent);
+    }
+
+    private boolean isGetKey = false;
+    private String KEY_Activity_Parames = null;
+
+    public String getKEY_Activity_Parames() {
+        if (isGetKey == false && getIntent() != null && getIntent().getExtras() != null && getIntent().getExtras().containsKey(KEY_Activity_Parames_Cache)) {
+            KEY_Activity_Parames = getIntent().getExtras().getString(KEY_Activity_Parames_Cache);
+            isGetKey = true;
+        }
+        return KEY_Activity_Parames;
+    }
+
+    private Object activityParames = null;
+
+    public <T> T getActivityParames() {
+        if (activityParames == null && getKEY_Activity_Parames() != null) {
+            activityParames = getApp().getParamsFromJumpCache(getKEY_Activity_Parames());
+        }
+        return (T) activityParames;
+    }
+
+
+    //********************************************
+    //            startActivityForResult
+    //                  反向传值处理
+    //********************************************
+    // 逆向传值key
+    public static final String KEY_Activity_Parames_Cache_Reverse = "Activity_Parames_Cache_Reverse_9f080b39-f811-4da1-b7aa-a2fdc66dd5c3";
+
+    public void startActivityForResult(Intent intent, int requestCode, Object parames) {
+        String key = getApp().putParamsToJumpCache(parames);
+        intent.putExtra(KEY_Activity_Parames_Cache, key);
+        startActivityForResult(intent, requestCode);
+    }
+
+    public void setResult(int resultCode, Object reverseParames) {
+        Intent intent = new Intent();
+        String key = getApp().putParamsToReverseCache(reverseParames);
+        intent.putExtra(KEY_Activity_Parames_Cache_Reverse, key);
+        setResult(resultCode, intent);
+    }
+
+    public void setResult(int resultCode, Intent data, Object reverseParames) {
+        String key = getApp().putParamsToReverseCache(reverseParames);
+        data.putExtra(KEY_Activity_Parames_Cache_Reverse, key);
+        setResult(resultCode,data);
+    }
+
+    /**
+     * 一个  intent 只能取一次,,,
+     * @param data
+     * @param <T>
+     * @return
+     */
+    public <T> T getReverseParames(Intent data){
+        if (data != null && data.getExtras() != null && data.getExtras().containsKey(KEY_Activity_Parames_Cache_Reverse)){
+            String key = data.getExtras().getString(KEY_Activity_Parames_Cache_Reverse);
+            return getApp().removeParamsFromReverseCache(key);
+        }
+        return null;
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        onActivityResult(requestCode,resultCode,data,getReverseParames(data));
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data, Object reverseParames) {
+
+    }
 
 
 }
--- src/main/java/com/rdxer/xxlibrary/base/BaseApplication.java
+++ src/main/java/com/rdxer/xxlibrary/base/BaseApplication.java
@@ -5,6 +5,11 @@ import android.app.Application;
 import com.litesuits.orm.LiteOrm;
 import com.rdxer.xxlibrary.utils.HTTPUtils;
 
+import java.util.Map;
+import java.util.Stack;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+
 /**
  * Created by LXF on 16/6/20.
  */
@@ -35,6 +40,87 @@ public class BaseApplication extends Application {
         this.db = db;
     }
 
+
+    //********************************************
+    //               正向传值处理
+    //********************************************
+    // 页面跳转的参数缓存
+    private Map<String,Object> jumpCache;
+    public synchronized Map<String, Object> getJumpCache() {
+        if (jumpCache == null) {
+            jumpCache = new ConcurrentHashMap<>();
+        }
+        return jumpCache;
+    }
+
+    /**
+     * 页面参数 缓存put
+     * @param params 页面参数
+     * @return 缓存的key
+     */
+    public synchronized String putParamsToJumpCache(Object params){
+        String uuid = UUID.randomUUID().toString();
+        getJumpCache().put(uuid,params);
+        return uuid;
+    }
+
+    /**
+     * 获取缓存
+     * @param key 添加时候返回的可以
+     * @return 添加时候的缓存
+     */
+    public synchronized <T>  T getParamsFromJumpCache(String key){
+        T t = (T) getJumpCache().get(key);
+        return t;
+    }
+
+    /**
+     * 删除缓存 使用 key
+     * @param key key
+     * @return 获取删除的缓存
+     */
+    public synchronized <T>  T removeParamsFromJumpCache(String key){
+        T t = (T) getJumpCache().remove(key);
+        return t;
+    }
+
+    //********************************************
+    //               反向传值处理 Reverse
+    //********************************************
+    // 页面跳转的参数缓存
+    private Map<String,Object> reverseCache;
+
+    public synchronized Map<String, Object> getReverseCache() {
+        if (reverseCache == null) {
+            reverseCache = new ConcurrentHashMap<>();
+        }
+        return reverseCache;
+    }
+
+    /**
+     * 逆向传值 缓存
+     * @param params 逆向传值
+     * @return 缓存的key
+     */
+    public synchronized String putParamsToReverseCache(Object params){
+        String uuid = UUID.randomUUID().toString();
+        getReverseCache().put(uuid,params);
+        return uuid;
+    }
+    /**
+     * 删除缓存 使用 key
+     * @param key key
+     * @return 获取删除的缓存
+     */
+    public synchronized <T>  T removeParamsFromReverseCache(String key){
+        T t = (T) getReverseCache().remove(key);
+        return t;
+    }
+
+
+    //********************************************
+    //                生命周期
+    //********************************************
     @Override
     public void onCreate() {
         super.onCreate();
